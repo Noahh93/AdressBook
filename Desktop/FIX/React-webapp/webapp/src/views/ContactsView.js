f9bd03c43e5c8952e226127ce896d82976e4a665
Noahh93 <Noaf_33@hotmail.com>
2022-10-31 21:15:50 -0500

    before responsive design for mainmenusection

---
+++ Desktop/FIX/React-webapp/webapp/src/views/ContactsView.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainMenuSection from '../sections/MainMenuSection'
+import FooterSection from '../sections/FooterSection'
+
+const ContactsView = () => {
+  return (
+    <>
+      <MainMenuSection />
+      <FooterSection />
+    <h1>Contacts</h1>
+  </>
+  )
+}
+
+export default ContactsView
---
+++ Desktop/FIX/React-webapp/webapp/src/views/HomeView.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import MainMenuSection from '../sections/MainMenuSection'
+import FooterSection from '../sections/FooterSection'
+
+const HomeView = () => {
+  return (
+    <>
+      <MainMenuSection />
+      <FooterSection />
+    </>
+  )
+}
+
+
+
+
+export default HomeView
